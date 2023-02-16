fbd68707c23eff7cd5b0f31b86ffb08322c42cc0
ozandalee <ozandalee@gmail.com>
2023-02-16 13:05:28 -0800

    Helloputs

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 /**
- * main - Entry
- * Return: Always 0
- */
+* main-Entry point
+* Return: 0 if successful
+*/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
+
