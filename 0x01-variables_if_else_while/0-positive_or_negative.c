ef477d63a4191292035c917512fd8070c3b2b7e0
ozandalee <ozandalee@gmail.com>
2023-02-17 19:23:57 -0800

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *  * main -Entry point
+ *   * description : Positive anything is better than negative nothing
+ *    * Return : Always 0 (success)
+ *     * */
+int main (void)
+{
+	        n = rand() - RAND_MAX / 2;
+			        if (n>0)
+					        {
+							                printf("is is positive\n");
+									                                else(n<0)
+														                                printf("is negative\n");
+													                                else
+																		                                printf("is zero\n");
+																	        }
+return (0);
+}
+
